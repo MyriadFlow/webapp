619a17bb2badee279eab08656cf09cc9c07c1fe8
thisisommore <ommore501@gmail.com>
2022-02-17 17:16:22 +0000

    Updated contract address

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const marketplaceAddress = "0xA3c64851558D85449B9C8aA88b217BE7Af9d7eFB"
-export const creatifyAddress = "0x975362c36b6842d48d02DBD3A077745Fc1C64175"
+export const marketplaceAddress = "0x8E3a69e094C72Aaca55191A91BAEbE9F800bD908"
+export const creatifyAddress = "0xA34D5022e7fa813F460eB9eF8E59c3386faCE489"
