a0dbdd9418deb5e242aec9bd1006e79e880147af
DEVSI SINGH <grace.emily876@gmail.com>
2022-02-25 07:08:58 +0000

    graphql api endpoint changed

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://api.thegraph.com/subgraphs/name/thisisommore/market-place-subgraph",
+    // uri: "https://api.thegraph.com/subgraphs/name/thisisommore/market-place-subgraph",
+    uri:"https://api.thegraph.com/subgraphs/name/thisisommore/myriad-flow",
     cache: new InMemoryCache(),
 });
 
