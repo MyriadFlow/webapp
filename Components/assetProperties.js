290b9ce89ba32923fcd7f57624cd7129924584a6
DEVSI SINGH <grace.emily876@gmail.com>
2022-03-01 14:51:41 +0000

    changes

--- Components/assetProperties.js
+++ Components/assetProperties.js
@@ -22,6 +22,7 @@ const assetProperties = ({ uri }) => {
             {response.length > 0 ? response.map((item) => {
 
                 // console.log(item);
+                const width = item.value;
 
                 return (<div
                     key={item.id}
@@ -31,9 +32,13 @@ const assetProperties = ({ uri }) => {
                             <p className="font-extralight">{item && item.trait_type}</p>
                         </div>
                     </div>
+                    {/* (
+                        item.display_type == boost_number && (
                     <div class="w-full bg-gray-200 rounded-full">
-                        <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{ width: '25%' }}> 25%</div>
-                    </div>
+                        <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{ width: `${width}%` }}> {width}%</div>
+                    </div>)
+                    ) */}
+                    
                 </div>)
             }) : null}
         </div>
