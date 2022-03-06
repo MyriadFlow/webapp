62149c1f9987da815b34461df588d3159b9a8ae2
DEVSI SINGH <grace.emily876@gmail.com>
2022-03-06 07:07:57 +0000

    my artifacts graphql integration

--- pages/my-artifacts.js
+++ pages/my-artifacts.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import Web3Modal from "web3modal"
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
-import {FaEthereum} from "react-icons/fa"
+import { FaEthereum } from "react-icons/fa"
 
 import {
   marketplaceAddress, creatifyAddress
@@ -14,8 +14,41 @@ import {
 import Creatify from '../artifacts/contracts/Creatify.sol/Creatify.json'
 import Marketplace from '../artifacts/contracts/Marketplace.sol/Marketplace.json'
 import Layout from "../Components/Layout";
+import { selectUser } from "../slices/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import HomeComp from "../Components/homeComp";
+import HomeComp2 from "../Components/homecomp2";
 
-export default function MyAssets() {
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+    query Query($where: MarketItem_filter) {
+      marketItems(first: 20, where: {seller: "0x313bfad1c87946bf893e2ecad141620eaa54943a"}) {
+        price
+        itemId
+        seller
+        forSale
+        tokenId
+        metaDataUri
+      }
+    }
+    `,
+  });
+
+  return {
+    props: {
+      marketItems: data.marketItems,
+    },
+  };
+}
+
+export default function MyAssets({ marketItems}) {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  console.log(user);
   const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
   useEffect(() => {
@@ -29,49 +62,48 @@ export default function MyAssets() {
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
-      
+
     const marketContract = new ethers.Contract(marketplaceAddress, Marketplace.abi, signer)
     const tokenContract = new ethers.Contract(creatifyAddress, Creatify.abi, provider)
-    setLoadingState('loaded') 
+    setLoadingState('loaded')
   }
   return (
     <Layout>
       <div className="flex justify-center min-h-screen">
-      <div className="p-4 pt-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            // nfts.map((nft, i) => (
-              // <div key={i} className="border shadow-md rounded-xl overflow-hidden hover:scale-105 transition transform ease-out duration-200 cursor-pointer group ">
-              //   <img src={nft.image} className="rounded p-2" />
-              //   <div className="p-4 bg-black  group-hover:bg-white flex justify-center ">
-              //     <p className="text-md font-bold text-white  group-hover:text-black ">Price -</p> 
-              //     <div className="flex">
-              //     <FaEthereum className="h-6 w-6 text-white group-hover:text-black "/>
-              //     <p className="text-white text-md  group-hover:text-black ">{nft.price} Eth</p>
-              //     </div>
-                 
-                  
-              //   </div>
-              // </div>
-              <div
-						// key={i} 
-						className="border shadow-md rounded-xl overflow-hidden hover:scale-105 transition transform ease-out duration-200 cursor-pointer group ">
-						<img src="https://www.forbes.com/advisor/wp-content/uploads/2021/04/NFT.jpeg-900x510.jpg" className="rounded p-2 h-44 w-full" />
-						<div className="p-4 bg-blue-500 dark:bg-black group-hover:bg-gray-200 flex justify-center ">
-							<p className="text-md font-bold text-white group-hover:text-black ">Price -</p>
-							<div className="flex">
-								<FaEthereum className="h-6 w-6 text-white group-hover:text-black " />
-								<p className="text-white text-md  group-hover:text-black">0.1 ETH</p>
-							</div>
+        <div className="p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-4 lg:gap-24 p-4 mt-20  h-auto">
+            {marketItems.map((item) => {
+
+              console.log(item);
+              console.log(item.metaDataUri.substr(7, 50));
+
+              return (<div
+                key={item.itemId}
+                className="bg-white dark:bg-gray-900  rounded-lg shadow-lg w-full lg:w-72 hover:scale-105 duration-200 transform transition cursor-pointer border-2 dark:border-gray-800">
+                <HomeComp uri={item ? item.metaDataUri.substr(7, 50) : ""} />
 
 
-						</div>
-					</div>
-          //   ))
-          }
+                <div className="flex px-4 py-6">
+                  <HomeComp2 uri={item ? item.metaDataUri.substr(7, 50) : ""} />
+
+                </div>
+                <div className=" flex items-center justify-between px-4 mb-2">
+                  <p className="font-1 text-sm font-bold">Price </p>
+                  <div className="flex items-center">
+                    <FaEthereum className="h-4 w-4 text-blue-400" />
+                    <p className="font-extralight dark:text-gray-400">{item.price}</p>
+                  </div>
+                </div>
+
+                <div className="px-4 py-4 bg-gray-100 dark:bg-gray-700 flex justify-between">
+                  <button onClick={() => buyNft(nft)} className="text-blue-500 hover:text-blue-400 font-bold">Buy now</button>
+                </div>
+
+              </div>)
+            })}
+          </div>
         </div>
       </div>
-    </div>
-    </Layout> 
+    </Layout>
   )
 }
