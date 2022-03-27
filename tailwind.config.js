324b00bed58f2c4f9c1b6bc2025d792f8cc80bab
Manish Kushwaha <manishgtx1050@gmail.com>
2022-03-27 17:59:35 +0530

    Home Page Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   mode: "jit",
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'opensans': ['"Open Sans"', 'sans-serif']
+      },
+      colors: {
+        'fontblue': 'hsl(243, 87%, 12%)',
+      },
+    },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
