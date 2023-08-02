e0e9db5c989741a8e3afc3588066e8999938006d
Devsi <grace.emily876@gmail.com>
2023-08-02 13:22:24 +0530

    drops page alignment

--- pages/drops.js
+++ pages/drops.js
@@ -10,21 +10,21 @@ export default function Rewards() {
 
   return (
    <Layout title="Drops"description="This is used to show the drops info">
-    <div className='flex body-back'>
-<div>
+    <div className='flex body-back mx-auto items-center justify-center lg:flex-row flex-col'>
+<div className='lg:mb-16'>
   <img alt="alt"  src="/imagere.png"/>
 </div>
-<div> 
-  <div className='reward-text text-4xl font-bold mt-20'> Airdrop Alert</div>
-  <div className='mt-20'>
-    <div className='text-center text-2xl font-bold text-gray-500 dark:text-white'>Claim Your NFTs </div>
-    <div className='text-center text-2xl font-bold text-gray-500 dark:text-white'>Now!</div>
+<div className='items-center flex flex-col'> 
+  <div className='reward-text text-4xl font-bold lg:mt-18 md:-mt-20'> Airdrop Alert</div>
+  <div className='mt-16'>
+    <div className='text-center text-2xl font-bold text-gray-500 dark:text-white'>Claim Your NFTs Now!</div>
+    {/* <div className='text-center text-2xl font-bold text-gray-500 dark:text-white'>Now!</div> */}
 </div>
-<div className='text-gray-500 dark:text-white text-sm mt-10'>Currently You Do Not Have Access To </div>
-<div className='text-gray-500 dark:text-white text-sm'>The Creator Role. To Gain Access,......</div>
+<div className='text-gray-500 dark:text-gray text-sm mt-10'>Currently you do not meet the</div>
+<div className='text-gray-500 dark:text-gray text-sm mb-10'>eligibility requirements</div>
 
-<div className='mt-10 text-center text-gray-500 dark:text-white'>Stay Tunned For Upcoming Drops</div>
-<div className='text-center mt-10 bg-white text-black px-4 py-2 rounded-full ... text-sm'>
+<div className='mt-10 text-2xl text-center text-gray-500 dark:text-white'>Stay Tunned For Upcoming Drops</div>
+<div className='text-center mt-10 mb-10 bg-white text-black px-10 py-3 rounded-full ... text-sm'>
   <Link className="rewards-style" href="/explore">
               <NavLink
                 className={router.pathname == "/explore" ? "active " : ""}
