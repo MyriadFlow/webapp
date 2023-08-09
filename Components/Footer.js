69c8758e16209dcf77765e8d6823154bbf976f5a
Devsi <grace.emily876@gmail.com>
2023-08-09 16:06:21 +0530

    link tag added

--- Components/Footer.js
+++ Components/Footer.js
@@ -83,7 +83,7 @@ const Footer = () => {
                             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
-                                    <a href="/about" class="hover:underline">About</a>
+                                    <Link href="/about" class="hover:underline">About</Link>
                                 </li>
                                 <li class="mb-4">
                                     <a href="#" class="hover:underline">Terms &amp; Conditions</a>
