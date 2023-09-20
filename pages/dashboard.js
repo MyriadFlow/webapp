40568eaad7bca2db2f1802c46a050f49699d2bcf
DEVSI SINGH <grace.emily876@gmail.com>
2023-09-20 19:19:40 +0000

    dashboard pop up

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -28,6 +28,7 @@ export default function CreatorDashboard() {
 
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState("bought");
+  const [showModal, setShowModal]=useState(false);
 
   const fetchUserAssests = async (walletAddr) => {
     const query = gql`
@@ -227,6 +228,85 @@ export default function CreatorDashboard() {
         )}
         {page === "bought" && (
           <div className="p-4 px-10">
+            {showModal ? (
+                <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none dark:body-back body-back-light">
+
+
+                        <div className="py-12 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
+                            <div role="alert" className="container mx-auto w-2/3 rounded-lg">
+                                <div className="relative py-4 bg-white shadow-md rounded border border-gray-400 rounded-2xl">
+                                    <div className="w-full flex justify-start text-gray-600 mb-3">
+                                        <button onClick={() => setShowModal(false)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x mr-4 ml-4" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" />
+                                                <line x1="18" y1="6" x2="6" y2="18" />
+                                                <line x1="6" y1="6" x2="18" y2="18" />
+                                            </svg>
+                                        </button>
+
+                                        <h3 className="text-2xl font-semibold text-gray-900 ml-6">
+                                        Choose a type of Sale
+                                        </h3>
+                                    </div>
+
+                                    <div class="flex p-10 ml-10">
+                                        {/* <input type="file" className="btn btn-primary btn-md ml-36" style={{ marginBottom: 20, marginTop: 20, width: "50%" }} onChange={(e) => { uploadImage(e) }} /> */}
+                                        <div className="w-1/2">
+                                        <h3 className="text-2xl font-semibold text-gray-900">
+                                        Fixed Price
+                                        </h3>
+                                        <p className="font-semibold text-gray-900">
+                                        the item is listed at the price you set.
+                                        </p>
+                                        <h3 className="text-2xl font-semibold text-gray-900">
+                                        Sell to higher bidder
+                                        </h3>
+                                        <p className="font-semibold text-gray-900">
+                                        the item is listed for auction.
+                                        </p>
+                                        <h3 className="text-2xl font-semibold text-gray-900">
+                                        Set a price
+                                        </h3>
+                                        <p className="font-semibold text-gray-900">Starting Price</p>
+                                        <div className="mb-2">
+                                            <label for="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">instagram URL (Optional)</label>
+                                            <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                        </div>
+                                        <p className="font-semibold text-gray-900">Duration</p>
+                                        <div className="mb-2">
+                                            <label for="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">instagram URL (Optional)</label>
+                                            <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                        </div>
+                                        <button
+                                        className="text-white bg-blue-500 text-sm px-8 py-3 rounded-full border border-white shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                    >
+                                        Add
+                                    </button>
+                                        </div>
+                                        <div class="w-1/3 ml-10">
+                                        <img src="/vr.png" className="rounded-lg"/>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+            ) : null}
+
+            {/* <div className="flex items-center md:justify-end lg:-my-16 lg:mx-8 md:-my-16 md:mx-8 mt-8 justify-center lg:justify-end">
+                    <button
+                        className=" text-white text-sm px-8 py-3 rounded-full border border-white shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => setShowModal(true)}
+                    >
+                        Edit Profile
+                    </button>
+                </div> */}
             <BoughtItems />
           </div>
         )}
