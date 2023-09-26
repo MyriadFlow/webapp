55630cd441f53d41b7b19d96b2c543069dff3988
Devsi <grace.emily876@gmail.com>
2023-09-26 19:36:10 +0530

    asset page ui responsive

--- pages/collections/[signatureseries]/[id]/[nftid].js
+++ pages/collections/[signatureseries]/[id]/[nftid].js
@@ -104,7 +104,7 @@ function Token() {
     <Layout>
       <div className="min-h-screen p-8 mx-auto bg-[#f8f7fc] dark:bg-[#131417] dark:body-back body-back-light">
         <div className="flex flex-col lg:flex-row">
-          <div className="w-1/3 border border-gray-600 p-4 mr-4">
+          <div className="lg:w-1/3 border border-gray-600 p-4 mr-4">
             <AssetComp
               uri={data ? data?.metaDataURI : ""}
             />
@@ -112,7 +112,7 @@ function Token() {
             <div>{nftDatas?.description}</div>
           </div>
 
-          <div className="w-2/3">
+          <div className="lg:w-2/3 w-full">
 
             <div className="pl-10 border border-gray-600 p-4 mb-4">
               {
@@ -146,18 +146,18 @@ function Token() {
               {/* { data && data.price && (
                             <h2 className="font-bold text-xl">{getEthPrice(data?.price)} MATIC</h2>
                             ) } */}
-              <div className="flex">
+              <div className="lg:flex md:flex">
                 <button
                   onClick={() =>
                     buyItem(data, 1)
                   }
-                  className="flex gap-x-2 items-center justify-center px-10 py-3 my-4 text-sm font-medium rounded-lg bg-white text-black"
+                  className="flex gap-x-2 items-center justify-center lg:px-10 md:px-10 px-3 py-3 my-4 text-sm font-medium rounded-lg bg-white text-black"
                 >
                   <span className="text-lg font-bold">Buy Now</span>
                   <BiWallet className="text-3xl" />
                 </button>
                 <button
-                  className="flex gap-x-2 items-center justify-center px-10 py-3 m-4 text-sm font-medium rounded-lg text-white border"
+                  className="flex gap-x-2 items-center justify-center lg:px-10 md:px-10 px-3 py-3 lg:m-4 md:m-4 text-sm font-medium rounded-lg text-white border"
                 >
                   <BiWallet className="text-3xl" />
                   <span className="text-lg font-bold">Make an Offer</span>
@@ -170,14 +170,14 @@ function Token() {
               <div className="pt-10 pb-4 font-bold text-xl">Rental Duration</div>
               <div className="pb-10">Price</div>
 
-              <div className="flex">
+              <div className="lg:flex md:flex">
                 <button
-                  className="flex gap-x-2 items-center justify-center px-10 py-3 my-4 text-sm font-medium rounded-lg text-white border"
+                  className="flex gap-x-2 items-center justify-center lg:px-10 md:px-10 px-3 py-3 my-4 text-sm font-medium rounded-lg text-white border"
                 >
                   <span className="text-lg font-bold">Months Days Hours</span>
                 </button>
                 <button
-                  className="flex gap-x-2 items-center justify-center px-10 py-3 m-4 text-sm font-medium rounded-lg text-white bg-blue-700"
+                  className="flex gap-x-2 items-center justify-center lg:px-10 md:px-10 px-3 py-3 lg:m-4 md:m-4 text-sm font-medium rounded-lg text-white bg-blue-700"
                 >
                   <BiWallet className="text-3xl" />
                   <span className="text-lg font-bold">Rent Now</span>
