94cbaa093afc9d99b063bc48ca227c803b3bf7da
Devsi <grace.emily876@gmail.com>
2023-09-27 16:27:52 +0530

    footer light and dark mode

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -220,7 +220,7 @@ export default function Marketplace() {
                   <Typewriter
             strings={[
               'Community',
-              'Organiser',
+              'Organization',
               'Businesses',
               'Audience'
             ]}
