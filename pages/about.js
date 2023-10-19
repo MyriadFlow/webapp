7a855c7259260381df430a266df9276de744ed50
Devsi <grace.emily876@gmail.com>
2023-10-19 23:50:04 +0530

    about page changes

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import { NavLink } from "reactstrap";
 import Layout from "../Components/Layout";
 import { useRouter } from "next/router";
 import Image from 'next/image';
+import AssetComp from "../Components/assetComp";
 import { useData } from "../context/data";
 
 export default function About() {
@@ -17,28 +18,28 @@ export default function About() {
       description="All the information you want to know about NFT and the company is here."
     >
       <div className="min-h-screen dark:body-back body-back-light">
-        <div className="w-4/5 mx-auto p-28 border rounded-lg" style={{backgroundColor: '#FFFFFF'}}> image</div>
+        <div className="w-4/5 mx-auto pt-10 rounded-lg"> 
+        <AssetComp
+              uri={resdata?.Storefront.relevantImage} class="mx-auto"/>
+        {/* <img src={resdata?.Storefront.relevantImage} class="mx-auto"/> */}
+        </div>
         <div className="text-center text-black dark:text-white">
-<div className="text-3xl font-bold pt-10">What we do</div>
-<div className="text-xl lg:w-1/3 md:w-1/3 mx-auto mt-4 mb-20">Lorem ipsum dolor sit amet. Et odit quibusdam est 
-saepe eaque qui enim reprehenderit quo voluptas 
-rerum. Sed voluptate possimus aut quidem 
-molestias cum velit enim rem maiores temporibus.</div>
+<div className="text-3xl font-bold pt-10">{resdata?.Storefront.personalTagline}</div>
+<div className="text-xl lg:w-1/3 md:w-1/3 mx-auto mt-4 mb-20">{resdata?.Storefront.personalDescription}</div>
 </div>
       <div className="flex mx-auto items-center justify-center lg:flex-row flex-col lg:px-32 px-10 pb-60">
         
         <div className="lg:w-1/2 w-full mt-6">
-          <img src={resdata?.Storefront.relevantImage} alt="Picture of the owner" />
+          {/* <img src={resdata?.Storefront.relevantImage} alt="Picture of the owner" /> */}
+          <AssetComp
+              uri={resdata?.Storefront.profilePictureUrl}
+            />
         </div>
         <div className="lg:w-1/2 text-black dark:text-white">
-          <div className="text-3xl font-bold">Name</div>
+          <div className="text-3xl font-bold">{resdata?.profile.name}</div>
           <div className="text-3xl mt-10">Profession</div>
-          <div className="mt-16">Alex Turner is a visionary e-commerce entrepreneur with a passion for creating innovative
-            online marketplaces. With a background in computer science and a knack for identifying
-            market trends, Alex founded their first successful online marketplace at the age of 25.
-            They are committed to fostering a vibrant community of buyers and sellers, empowering small
-            businesses, and driving economic growth through their platforms.</div>
-          <div className="mt-4">Seattle, Washington, USA</div>
+          <div className="mt-16">{resdata?.profile.bio}</div>
+          <div className="mt-4">{resdata?.profile.location}</div>
           {/* <div
             className="text-3xl font-bold mt-10 text-gray-500 dark:text-green-400 text-green-400"
           >
